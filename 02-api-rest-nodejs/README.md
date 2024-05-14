7bc4df9ce29b472e8adaf7b06e803b7961e81a05
Caio Ceretta <caioceretta@gmail.com>
2024-05-14 11:53:19 -0300

    Up to cookie creation and addition in new transaction

--- 02-api-rest-nodejs/README.md
+++ 02-api-rest-nodejs/README.md
@@ -3,21 +3,21 @@
 Detailed descriptions of functionalities or services that a software system must provide. They describe the behavior of
 the system under specific conditions and are usually described in terms of inputs, outputs, and expected behavior.
 
-- [ ] User must be able to create a new transaction
-- [ ] User must be able to get a summary of his account
-- [ ] User must be able to list all the transactions that already occurred
-- [ ] User must be able to visualize a single transaction
+- [ X ] User must be able to create a new transaction
+- [ X ] The user must be able to get a summary of his account
+- [ X ] The user must be able to list all the transactions that already occurred
+- [ X ] The user must be able to visualize a single transaction
 
 # Business Rules
 
 Guidelines or constraints that define or regulate the operations of an organization. They are specific to each business
 and typically describe how processes should be executed or how data should be handled.
 
-- [ ] The transaction can be of type credit which will sum to the total value, or debt that will subtract from it
+- [ X ] The transaction can be of type credit which will sum to the total value, or debt that will subtract from it
 - [ ] It should be able for us to identify the user among the requests
-- [ ] User must be allowed to visualize only the transactions created by him
+- [ ] The user must be allowed to visualize only the transactions created by him
 
 # Non-functional Requirements
 
 Criteria that describe the qualities or characteristics that a software system must have but are not directly related to
-the specific functionalities of the system. This includes requirements for performance, security, usability, among others.
+the specific functionalities of the system. This includes requirements for performance, security, and usability, among others.
--- 02-api-rest-nodejs/src/routes/transactions.ts
+++ 02-api-rest-nodejs/src/routes/transactions.ts
@@ -1,13 +1,50 @@
 import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { knex } from '../database'
-import crypto from 'node:crypto'
+import crypto, { randomUUID } from 'node:crypto'
+
+/*
+  Cookies <-> ways for us to keep a context between the requisitions
+
+  For example, we don't need to be logged in some social media for it to know who we are in the context of all the requests
+  that we do. It means that by the moment that we access some of these sites, he will save some information as an id inside
+  our browser, in a way that we don't even notice (after we accept the lgpd agreement), even if we are not logged, the id
+  it stores, is a way of the application to be able to validate that the same person, based on the cookies ID, made the requests
+  inside the application.
+
+  Once we log in, all that historic of things we've done, even though we were not logged in, are saved in our account.
+
+  Now, we are use going to use cookies for us to know that the same user, which is creating transactions, is the user that
+  is listing the transactions
+*/
 
 export async function transactionRoutes(app: FastifyInstance) {
   app.get('/', async () => {
     const transactions = await knex('transactions').select()
 
-    return transactions
+    return { transactions }
+  })
+
+  app.get('/:id', async (req) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    console.log(req.params)
+
+    const { id } = getTransactionsParamsSchema.parse(req.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return transaction
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return { summary }
   })
 
   app.post('/', async (req, rep) => {
@@ -19,10 +56,22 @@ export async function transactionRoutes(app: FastifyInstance) {
 
     const { title, amount, type } = createTransactionBodySchema.parse(req.body)
 
+    let sessionId = req.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      rep.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 Days
+      })
+    }
+
     await knex('transactions').insert({
       id: crypto.randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return rep.status(201).send()
